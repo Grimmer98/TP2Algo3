4d90bf0c5afd2492d773a8a8585e81a0836872a4
Matias Fernandez <matiasfdez@outlook.com>
2023-05-17 15:37:56 -0300

    2 modificado

--- dominoes.cpp
+++ dominoes.cpp
@@ -1,34 +1,36 @@
 #include <vector>
-#include <iostream>
 #include <stdio.h>
+#include <stack>
+#include <list>
 
 using namespace std;
 
 enum { WHITE = 0, GREY = 1, BLACK = 2 };
-vector<int> stack, state, stateT, stateFinal;
+stack<int> dfs_stack;
+vector<int> stateInitial, stateTransposed, stateFinal;
 vector<vector<int>> edges, edgesT;
-vector<int> cfcs;
+list<int> cfcs;
 vector<bool> sol;
 
 void transposed_dfs(int v){
-    stateT[v] = GREY;
+    stateTransposed[v] = GREY;
     
     for (int u : edgesT[v])
-        if (stateT[u] == WHITE)
+        if (stateTransposed[u] == WHITE)
             transposed_dfs(u);
     
-    stateT[v] = BLACK;
+    stateTransposed[v] = BLACK;
 }
 
 void initial_dfs(int v) {
-    state[v] = GREY;
+    stateInitial[v] = GREY;
     
     for (int u : edges[v])
-        if (state[u] == WHITE)
+        if (stateInitial[u] == WHITE)
             initial_dfs(u);
     
-    stack.insert(stack.begin(), v);
-    state[v] = BLACK;
+    dfs_stack.push(v);
+    stateInitial[v] = BLACK;
 }
 
 void final_dfs(int v){
@@ -41,22 +43,23 @@ void final_dfs(int v){
     stateFinal[v] = BLACK;
 }
 
-vector<int> minimalSet(){
-    vector<int> res;
+list<int> minimalSet(){
+    list<int> res;
 
-    for (int v = 0; v < state.size(); v++)
-        if (state[v] == WHITE)
+    for (int v = 0; v < stateInitial.size(); v++)
+        if (stateInitial[v] == WHITE)
             initial_dfs(v);
 
-    for (int v : stack){
-        if (stateT[v] == WHITE){
-            cfcs.push_back(v); transposed_dfs(v); 
+    while (!dfs_stack.empty()){
+        int v = dfs_stack.top(); dfs_stack.pop();
+        if (stateTransposed[v] == WHITE){
+            cfcs.push_back(v); transposed_dfs(v);
         }
     }
 
-    for (int v : cfcs){
-        if (stateFinal[v] == WHITE){
-            sol[v] = true; final_dfs(v);
+    for (auto it = cfcs.begin(); it != cfcs.end(); ++it){
+        if (stateFinal[*it] == WHITE){
+            sol[*it] = true; final_dfs(*it);
         }
     }
 
@@ -70,7 +73,7 @@ int main(int argc, char const *argv[]){
     int n, m;
     scanf("%d %d", &n, &m);
 
-    stateT = state = stateFinal = vector<int>(n, WHITE);
+    stateTransposed = stateInitial = stateFinal = vector<int>(n, WHITE);
     
     edges.resize(n);
     edgesT.resize(n);
@@ -83,7 +86,7 @@ int main(int argc, char const *argv[]){
         edgesT[w - 1].push_back(v - 1);
     }
 
-    vector<int> res = minimalSet();
+    list<int> res = minimalSet();
     printf("%li\n", res.size());
     for (int v : res){
         printf("%d ", v);
